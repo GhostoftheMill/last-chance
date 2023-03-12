9b0389b5f197be92230015787cac90460b96cb26
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-03-11 22:15:17 -0500

    Did todo on datebase js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,26 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// TODO: Add logic to a method that accepts some content and adds it to the database - DONE
+export const putDb = async (id, val) => {
+  const jateDB = await openDB('jate', 1);
+  const text = jateDB.transaction('jate', 'readwrite');
+  const storeOb = text.objectStore('jate');
+  const req = storeOb.put({ id: id, val: val });
+  const res = await req;
+  console.log('Date saved.', res);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+// TODO: Add logic for a method that gets all the content from the database - DONE
+export const getDb = async (val) => {
+  console.log('Getting data from jate database');
+  const jateDB = await openDB('jate', 1);
+  const text = jateDB.transaction('jate', 'readwrite');
+  const storeOb = text.objectStore.getAll();
+  const res = await req;
+  console.log('Data saved.', res);
+};
+
 
 initdb();
